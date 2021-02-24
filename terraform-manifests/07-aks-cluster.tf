197c7ebb15f51ef5a50415298d4490fe9ffbc994
SerhiyKozlov <serhiy.kozlov@infopulse.com>
2021-02-24 18:16:54 +0200

    add pool

--- terraform-manifests/07-aks-cluster.tf
+++ terraform-manifests/07-aks-cluster.tf
@@ -15,6 +15,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
     enable_auto_scaling  = true
     max_count            = 3
     min_count            = 1
+    node_count           = 1
     max_pods             = 30
     os_disk_size_gb      = 30
     type           = "VirtualMachineScaleSets"
