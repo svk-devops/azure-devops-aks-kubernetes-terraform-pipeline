f42e1457eee15ee2c9157ab2bcec921017c4fb32
SerhiyKozlov <serhiy.kozlov@infopulse.com>
2021-02-24 16:48:20 +0200

    add deploy aks

--- terraform-manifests/08-outputs.tf
+++ terraform-manifests/08-outputs.tf
@@ -34,16 +34,16 @@ output "azure_ad_group_objectid" {
 }
 
 
-# # Azure AKS Outputs
+# Azure AKS Outputs
 
-# output "aks_cluster_id" {
-#   value = azurerm_kubernetes_cluster.aks_cluster.id
-# }
+output "aks_cluster_id" {
+  value = azurerm_kubernetes_cluster.aks_cluster.id
+}
 
-# output "aks_cluster_name" {
-#   value = azurerm_kubernetes_cluster.aks_cluster.name
-# }
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks_cluster.name
+}
 
-# output "aks_cluster_kubernetes_version" {
-#   value = azurerm_kubernetes_cluster.aks_cluster.kubernetes_version
-# }
+output "aks_cluster_kubernetes_version" {
+  value = azurerm_kubernetes_cluster.aks_cluster.kubernetes_version
+}
